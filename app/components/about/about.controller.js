ae4bbef651cb27f3591aaf4a3dfe15abd82d332c
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-01 18:46:41 +0300

    add: switchable categories in about setction on index page

--- app/components/about/about.controller.js
+++ app/components/about/about.controller.js
@@ -5,11 +5,23 @@
     .controller('aboutController', aboutController);
 
   function aboutController($scope, $http, $window, $q, aboutService) {
-
     var vm = this;
-
+    
+    vm.displayItem = function(item) {
+      if(item){
+        item.selected = true;
+        
+        if(vm.selectedItem){
+          vm.selectedItem.selected = false;
+        }
+        
+        vm.selectedItem = item;
+      }
+    }
+    
     vm.data = aboutService.preparedData;
-
+    vm.displayItem(vm.data[0]);
+    
     return vm;
   }
 })();
