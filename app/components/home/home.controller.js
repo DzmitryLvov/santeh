a031bc4f7b1026de3f438c9a23cf683450ac0874
mrzand <zas15082015@gmail.com>
2016-06-06 12:46:05 +0300

    angular fix, fix html and css folder.

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -4,9 +4,9 @@
     angular.module('myApp')
         .controller('homeController', homeController);
 
-    homeController.$inject = ["$scope", "$http", "$window", "$q", "categoriesService"];
+    homeController.$inject = ["$scope", "$http", "$window", "$q"];
 
-    function homeController($scope, $http, $window, $q, categoriesService) {
+    function homeController($scope, $http, $window, $q) {
 
             var vm = this
             
