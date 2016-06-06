2b244b2eec4b9891656a46447e0d7d1c3f1c5b41
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-06 15:01:02 +0300

    add: empty module for news

---
+++ app/components/news/news.controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  angular.module('myApp')
+    .controller('newsController', newsController);
+
+  function newsController() {
+    var vm = this;
+    
+    
+    
+    return vm;
+  }
+})();
