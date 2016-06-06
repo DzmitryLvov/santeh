7d22e7b50d8ff963905cb364ab62b4b75549e2cd
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-06 19:41:35 +0300

    add: news preview block moved into directive.

--- app/components/news/news.controller.js
+++ app/components/news/news.controller.js
@@ -2,13 +2,22 @@
   'use strict';
 
   angular.module('myApp')
-    .controller('newsController', newsController);
+    .controller('newsController', newsController)
+    .directive('newsPreview', newsPreview);
 
   function newsController(newsService) {
     var vm = this;
-    
+
     vm.postList = newsService.getPostList();
-    
+
+    console.log(vm.postList);
     return vm;
-  }
+  };
+
+  function newsPreview() {
+    return {
+      restrict: 'E'
+      , templateUrl: 'app/components/news/views/news-preview.view.html'
+    };
+  };
 })();
