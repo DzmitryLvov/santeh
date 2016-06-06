3835a077cc49ddb9f30581f1a8e10b4bd7f9bdb9
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-06 12:23:46 +0300

    add: empty module for administration panel

---
+++ app/components/administration/administration.controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  angular.module('myApp')
+    .controller('administrationController', administrationController);
+
+  function administrationController() {
+    var vm = this;
+    
+    
+    
+    return vm;
+  }
+})();
