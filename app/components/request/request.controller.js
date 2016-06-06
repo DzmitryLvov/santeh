17f2808d248b453de0ff2b8b02eaf3f5974bd241
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-06 12:31:33 +0300

    add: empty module for request operations

---
+++ app/components/request/request.controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  angular.module('myApp')
+    .controller('requestController', requestController);
+
+  function requestController() {
+    var vm = this;
+    
+    
+    
+    return vm;
+  }
+})();
