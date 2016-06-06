e577f794c25e2fd5fb70f8892d332f3bb4ff43f9
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-06 15:44:01 +0300

    add: getting mock post list from news service

--- app/components/news/news.controller.js
+++ app/components/news/news.controller.js
@@ -4,10 +4,10 @@
   angular.module('myApp')
     .controller('newsController', newsController);
 
-  function newsController() {
+  function newsController(newsService) {
     var vm = this;
     
-    
+    vm.postList = newsService.getPostList();
     
     return vm;
   }
