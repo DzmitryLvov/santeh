edac5e86303e5d19d9ff4ba9650199f4a99d1a44
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-07 20:24:12 +0300

    add: news administration view and modal template to adding new post

--- app/components/administration/administration.controller.js
+++ app/components/administration/administration.controller.js
@@ -4,9 +4,30 @@
   angular.module('myApp')
     .controller('administrationController', administrationController);
 
-  function administrationController(newsService, aboutService, homeService, priceService, requestService) {
+  function administrationController(newsService, aboutService, homeService, priceService, requestService, $mdDialog, $mdMedia) {
     var vm = this;
-    
+
+    vm.postList = newsService.getPostList();
+    console.log(vm.postList);
+
+    vm.openNewPostDialog = function (event) {
+      var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+      
+      $mdDialog.show({
+          controller: DialogController
+          , templateUrl: 'app/components/administration/views/newpost.administration.view.html'
+          , parent: angular.element(document.body)
+          , targetEvent: event
+          , clickOutsideToClose: false
+          , fullscreen: useFullScreen
+        })
+        .then(function (answer) {
+          console.log(answer);
+        }, function () {
+          
+        });
+    }
+
     return vm;
   }
 })();
