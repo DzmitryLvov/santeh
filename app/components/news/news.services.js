f4c7b84af3fa4cce7f4fa995e5277a3619a516d0
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-10 12:30:57 +0300

    fix: edit news service to provide prepared array of posts instead of get function

--- app/components/news/news.services.js
+++ app/components/news/news.services.js
@@ -4,71 +4,58 @@
   var serviceId = 'newsService';
 
   angular.module('myApp').factory(serviceId, function newsService($http, $filter) {
-    var preparedData = [];
+    var self = this;
 
-    var factory = {
-      getPostList: getPostList
-      , getPost: getPost
+    var options = {
+      year: 'numeric'
+      , month: 'long'
+      , day: 'numeric'
+      , weekday: 'long'
     };
 
-    function getPostList() {
-      if (preparedData.length) {
-        return preparedData;
-      } else {
-        var options = {
-          year: 'numeric'
-          , month: 'long'
-          , day: 'numeric'
-          , weekday: 'long'
-        };
-
-        preparedData = [
-          {
-            id: 1
-            , title: 'Post title 1'
-            , date: new Date('12/12/15')
-            , previewText: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты."
-            , text: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана. Маленький ручеек Даль журчит по всей стране и обеспечивает ее всеми необходимыми правилами. Эта парадигматическая страна, в которой жаренные члены предложения залетают прямо в рот. Даже всемогущая пунктуация не имеет власти над рыбными текстами, ведущими безорфографичный образ жизни."
+    self.preparedData = [
+      {
+        id: 1
+        , title: 'Post title 1'
+        , date: new Date('12/12/15')
+        , previewText: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты."
+        , text: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана. Маленький ручеек Даль журчит по всей стране и обеспечивает ее всеми необходимыми правилами. Эта парадигматическая страна, в которой жаренные члены предложения залетают прямо в рот. Даже всемогущая пунктуация не имеет власти над рыбными текстами, ведущими безорфографичный образ жизни."
         }, {
-            id: 2
-            , title: 'Post title 2'
-            , date: new Date('11/11/15')
-            , previewText: "Однажды одна маленькая строчка рыбного текста по имени Lorem ipsum решила выйти в большой мир грамматики."
-            , text: "Однажды одна маленькая строчка рыбного текста по имени Lorem ipsum решила выйти в большой мир грамматики. Великий Оксмокс предупреждал ее о "
+        id: 2
+        , title: 'Post title 2'
+        , date: new Date('11/11/15')
+        , previewText: "Однажды одна маленькая строчка рыбного текста по имени Lorem ipsum решила выйти в большой мир грамматики."
+        , text: "Однажды одна маленькая строчка рыбного текста по имени Lorem ipsum решила выйти в большой мир грамматики. Великий Оксмокс предупреждал ее о "
         }, {
-            id: 3
-            , title: 'Post title 3'
-            , date: new Date('10/6/15')
-            , previewText: "Он собрал семь своих заглавных букв, подпоясал инициал за пояс и пустился в дорогу."
-            , text: "Он собрал семь своих заглавных букв, подпоясал инициал за пояс и пустился в дорогу. Взобравшись на первую вершину курсивных гор, бросил он последний взгляд назад, на силуэт своего родного города Буквоград, на заголовок деревни Алфавит и на подзаголовок своего переулка Строчка."
+        id: 3
+        , title: 'Post title 3'
+        , date: new Date('10/6/15')
+        , previewText: "Он собрал семь своих заглавных букв, подпоясал инициал за пояс и пустился в дорогу."
+        , text: "Он собрал семь своих заглавных букв, подпоясал инициал за пояс и пустился в дорогу. Взобравшись на первую вершину курсивных гор, бросил он последний взгляд назад, на силуэт своего родного города Буквоград, на заголовок деревни Алфавит и на подзаголовок своего переулка Строчка."
         }, {
-            id: 4
-            , title: 'Post title 4'
-            , date: new Date('9/8/15')
-            , previewText: "Взобравшись на первую вершину курсивных гор, бросил он последний взгляд назад"
-            , text: "Он собрал семь своих заглавных букв, подпоясал инициал за пояс и пустился в дорогу. Взобравшись на первую вершину курсивных гор, бросил он последний взгляд назад, на силуэт своего родного города Буквоград, на заголовок деревни Алфавит и на подзаголовок своего переулка Строчка."
+        id: 4
+        , title: 'Post title 4'
+        , date: new Date('9/8/15')
+        , previewText: "Взобравшись на первую вершину курсивных гор, бросил он последний взгляд назад"
+        , text: "Он собрал семь своих заглавных букв, подпоясал инициал за пояс и пустился в дорогу. Взобравшись на первую вершину курсивных гор, бросил он последний взгляд назад, на силуэт своего родного города Буквоград, на заголовок деревни Алфавит и на подзаголовок своего переулка Строчка."
         }];
-      }
 
-      function formatDate(element, index, array) {
-        element.formattedDate = element.date.toLocaleDateString("ru", options);
-        return true;
-      }
+    function formatDate(element, index, array) {
+      element.formattedDate = element.date.toLocaleDateString("ru", options);
+      return true;
+    }
 
-      preparedData.every(formatDate);
-
-      return preparedData;
-    };
+    self.preparedData.every(formatDate);
 
     function getPost(postId) {
-      if (!preparedData.length) {
-        getPostList();
+      if (!self.preparedData.length) {
+        //getPostList();
       }
 
       var id = parseInt(postId);
 
       if (id) {
-        var result = $filter('filter')(preparedData, {
+        var result = $filter('filter')(self.preparedData, {
           id: id
         }, true);
       }
@@ -76,6 +63,43 @@
       return result;
     };
 
-    return factory;
+    function savePost(post) {
+      if (post) {
+        var existingPost = getPost(post.id);
+        if (!existingPost) {
+          post.date = Date.today();
+          console.log(post.date);
+          post.formattedDate = post.date.toLocaleDateString("ru", options);
+
+          self.preparedData.push(post)
+        } else {
+          existingPost.title = post.title;
+          existingPost.previewText = post.previewText;
+          existingPost.text = post.text;
+          existingPost.date = post.date;
+        }
+      }
+    };
+
+    function deletePost(postId) {
+      var post = getPost(postId);
+
+      if (post) {
+        var index = self.preparedData.map(function (element) {
+          return element.id;
+        }).indexOf(postId);
+
+        if (index > -1) {
+          self.preparedData.splice(index, 1);
+        }
+      }
+    };
+
+    return {
+      preparedData: self.preparedData
+      , getPost: getPost
+      , savePost: savePost
+      , deletePost: deletePost
+    }
   });
 })();
