26b06358e742398bba6feb30c4e4d7840b0f9845
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-11 17:21:36 +0300

    fix: code refactoring in app.states.js

--- app/app.states.js
+++ app/app.states.js
@@ -4,53 +4,50 @@
  * https://github.com/angular-ui/ui-router/wiki
  */
 angular.module('myApp')
-    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
-
-    // any unknown URLS go to 404
-    $urlRouterProvider.otherwise('/404');
-    // no route goes to index
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider.when('', '/');
-    // use a state provider for routing
+
+    $urlRouterProvider.otherwise('/404');
 
     $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: 'app/components/home/views/home.view.html',
-            controller: "homeController",
-            controllerAs: 'ctrl'
-        })
-        .state('404', {
-            url: '/404',
-            templateUrl: 'app/shared/404.html'
-        })
-        .state('about', {
-            url: '/about',
-            templateUrl: 'app/components/about/views/about.view.html',
-            controller: 'aboutController',
-            controllerAs: 'ctrl'
-        })
-        .state('price', {
-            url: '/price',
-            templateUrl: 'app/components/price/views/price.view.html',
-            controller: 'priceController',
-            controllerAs: 'ctrl'
-        })
-        .state('news', {
-            url: '/news',
-            templateUrl: 'app/components/news/views/newslist.view.html',
-            controller: 'newsController',
-            controllerAs: 'ctrl'
-        })
-        .state('post', {
-            url: '/post/:postId',
-            templateUrl: 'app/components/news/views/post.view.html',
-            controller: 'postController',
-            controllerAs: 'ctrl'
-        })
-        .state('administration', {
-            url: '/administration',
-            templateUrl: 'app/components/administration/views/administration.view.html',
-            controller: 'administrationController',
-            controllerAs: 'ctrl'
-        });
+      .state('home', {
+        url: '/'
+        , templateUrl: 'app/components/home/views/home.view.html'
+        , controller: "homeController"
+        , controllerAs: 'ctrl'
+      })
+      .state('404', {
+        url: '/404'
+        , templateUrl: 'app/shared/404.html'
+      })
+      .state('about', {
+        url: '/about'
+        , templateUrl: 'app/components/about/views/about.view.html'
+        , controller: 'aboutController'
+        , controllerAs: 'ctrl'
+      })
+      .state('price', {
+        url: '/price'
+        , templateUrl: 'app/components/price/views/price.view.html'
+        , controller: 'priceController'
+        , controllerAs: 'ctrl'
+      })
+      .state('news', {
+        url: '/news'
+        , templateUrl: 'app/components/news/views/newslist.view.html'
+        , controller: 'newsController'
+        , controllerAs: 'ctrl'
+      })
+      .state('post', {
+        url: '/post/:postId'
+        , templateUrl: 'app/components/news/views/post.view.html'
+        , controller: 'postController'
+        , controllerAs: 'ctrl'
+      })
+      .state('administration', {
+        url: '/administration'
+        , templateUrl: 'app/components/administration/views/administration.view.html'
+        , controller: 'administrationController'
+        , controllerAs: 'ctrl'
+      });
 }]);
