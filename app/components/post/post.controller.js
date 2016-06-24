285aa691ce3f6528915baa530dda50c4bfcafe30
Dzmitry Lvov <Dzmitry_Lvov@epam.com>
2016-06-24 15:44:42 +0300

    fix: redirect to 404 when post not found

--- app/components/post/post.controller.js
+++ app/components/post/post.controller.js
@@ -4,13 +4,16 @@
   angular.module('myApp')
     .controller('postController', postController);
 
-  function postController(articleService, $stateParams) {
+  function postController(articleService, $state, $stateParams) {
     var vm = this;
 
     var searchResult = articleService.getPost($stateParams.postId);
 
     if (searchResult.length) {
       vm.post = searchResult[0];
+    }
+    else{
+      $state.go('404');
     }
 
     return vm;
